a1f691991b3f44644ca1fd4d7b3ee1012e176f5e
Alisha <alishakathaniya3@gmail.com>
2026-05-22 16:51:00 +0530

    video

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -12,7 +12,7 @@ export default function Hero({ asset }) {
 
   useEffect(() => {
     gsap.from(".hero-img", {
-      scale: 1.1, 
+      scale: 1.1,
       duration: 1,
       ease: "power2.out",
     });
@@ -27,19 +27,17 @@ export default function Hero({ asset }) {
   }, []);
 
   return (
-    <div className="h-screen relative">
-
+    <div className="h-[350] lg:h-screen relative">
       {/* FALLBACK IMAGE */}
       {!videoLoaded && (
         // <div
-         
+
         //   className="hero-img object-cover object-top z-0 flex justify-center items-center"
         // >
-         
+
         // </div>
         <div className="absolute inset-0 z-20 flex items-center justify-center ">
           <div className="flex flex-col items-center gap-4">
-
             {/* LOADER */}
             <FaCircleNotch className="animate-spin text-yellow-400 text-3xl" />
 
@@ -52,17 +50,17 @@ export default function Hero({ asset }) {
       )}
 
       {/* VIDEO */}
-     <video
-  className={`absolute w-full h-full object-cover object-top transition-opacity duration-700 ${
-    videoLoaded ? "opacity-100" : "opacity-0"
-  }`}
-  src={asset?.heroVideo || "/videoabout.mp4"}  // ✅ dynamic
-  autoPlay
-  muted
-  loop
-  playsInline
-  onLoadedData={() => setVideoLoaded(true)}
-/>
+      <video
+        className={`absolute w-full h-full object-cover object-top transition-opacity duration-700 ${
+          videoLoaded ? "opacity-100" : "opacity-0"
+        }`}
+        src={asset?.heroVideo || "/videoabout.mp4"} // ✅ dynamic
+        autoPlay
+        muted
+        loop
+        playsInline
+        onLoadedData={() => setVideoLoaded(true)}
+      />
       {/* DARK GRADIENT OVERLAY */}
       {/* <div className="absolute inset-0" /> */}
 
@@ -70,7 +68,7 @@ export default function Hero({ asset }) {
       <div className="hero-content absolute bottom-[-200px] sm:bottom-[-120px] w-full flex justify-center z-30">
         <div className="hero-box w-[90%] max-w-5xl bg-black/80 backdrop-blur-2xl rounded-3xl p-4 sm:p-8 border border-yellow-500/20 shadow-[0_0_60px_rgba(201,162,74,0.15)]">
           <h1 className=" text-sm lg:text-xl sm:text-2xl text-center mb-4 leading-snug">
-  {asset?.heroTitle || "Find Curated Properties Across the Globe"}
+            {asset?.heroTitle || "Find Curated Properties Across the Globe"}
           </h1>
 
           <SearchBar />
