c1ff5285509e2c28ea28dde10b9f9647a2258547
Akshay <Akshay Sharma>
2026-06-11 21:33:20 +0530

    change in src

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,13 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 import { Plus_Jakarta_Sans } from "next/font/google";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import { ToastContainer } from "react-toastify";
 import Layout from "./components/RootLayout";
+import { GoogleTagManager } from "@next/third-parties/google";
+
 const jakarta = Plus_Jakarta_Sans({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700"],
@@ -16,6 +19,9 @@ const jakarta = Plus_Jakarta_Sans({
 export const metadata: Metadata = {
   title: "Winstead",
   description: "Find Curated Properties Across the Globe",
+  verification: {
+    google: "nrq9MNsvP-2ARvNXGewA3MLtsKKjA6Imy7RYqFleH-Y",
+  },
 };
 
 export default function RootLayout({
@@ -24,21 +30,36 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    // <html lang="en" className={`${jakarta.variable}  h-full antialiased`}>
-    //   <body className="min-h-full flex flex-col">
-    //     <main className="bg-black text-white">
-    //       <Navbar />
-    //       {children}
-    //       <ToastContainer />
-    //       <Footer />
-    //     </main>
-    //   </body>
-    // </html>
-    <Layout className={`${jakarta.variable}  h-full antialiased`} >
-      <Navbar />
-          {children}
-          <ToastContainer />
-          <Footer />
-    </Layout>
+    <>
+      <Script id="gtm-script" strategy="afterInteractive">
+        {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];
+          w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+          var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+          j.async=true;
+          j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+          f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-NSC43PQQ');
+        `}
+      </Script>
+
+      <Layout className={`${jakarta.variable} h-full antialiased`}>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NSC43PQQ"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+
+        <Navbar />
+        {children}
+        <ToastContainer />
+        <Footer />
+        <GoogleTagManager gtmId="GTM-NSC43PQQ" />
+      </Layout>
+    </>
   );
 }
